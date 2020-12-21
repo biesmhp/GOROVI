1892cc419a2a6c56598949010e3f78c7ca500c38
moveh <carlos.gomezmovellan@iesmiguelherrero.com>
2020-12-21 17:24:17 +0100

    Bootstrap

--- codigo/portada.php
+++ codigo/portada.php
@@ -1,32 +1,96 @@
 <?php
 require_once "funciones.php";
-
-    
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" media="screen">
+    <!-- Compiled and minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Minified JS library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Compiled and minified Bootstrap JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="estilos.css">
     <title>GOROVI</title>
 </head>
+
 <body>
-    <nav>
-        <a href="productos.php">Productos</a>
-        <?php if(!isset($_SESSION["usuario"])) :?>
-            <a href="registro.php">Registrarse</a>
-            <a href="login.php">Inicio de sesion</a>
-        <?php endif ;?>
+
+    <nav class="navbar navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand px-5 volver" href="productos.php">Productos</a>
+        <?php if (!isset($_SESSION["usuario"])) : ?>
+            <a class="navbar-brand px-5 volver" href="registro.php">Registrarse</a>
+            <a class="navbar-brand px-5 volver" href="login.php">Iniciar sesión</a>
+        <?php endif; ?>
+        <?php if (isset($_SESSION["usuario"])) : ?>
+            <a class="navbar-brand px-5 volver" href="perfil.php"><?= "Perfil de: " . $_SESSION["usuario"]["usuario"] ?></a>
+            <?php if ($_SESSION["usuario"]["rol"] == "administrador") : ?>
+                <a class="navbar-brand px-5 volver" href="admin.php">Administración</a>
+                <a class="navbar-brand px-5 volver" href="almacen.php">Almacen</a>
+            <?php endif; ?>
+            <a class="navbar-brand px-5 volver" href="cesta.php">Cesta</a>
+        <?php endif; ?>
     </nav>
-    <?php if(isset($_SESSION["usuario"])) :?>
-        <a href="perfil.php"><?= "Perfil de: ".$_SESSION["usuario"]["usuario"]?></a>
-        <?php if($_SESSION["usuario"]["rol"]=="administrador") :?>
-            <a href="admin.php">Administración</a>
-            <a href="almacen.php">Almacen</a>
-        <?php endif ;?>
-        <a href="cesta.php">Cesta</a>
-    <?php endif ;?>
+
+    <!-- GALERIA PARA LAS CATEGORÍAS Y HACERLAS ENLACE FILTRADO-->
+    <div class="d-flex justify-content-center align-items-center" style="margin-top: 10%; margin-bottom:19%">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="fondo active"></li>
+                <li data-target="#myCarousel" data-slide-to="1" class="fondo"></li>
+                <li data-target="#myCarousel" data-slide-to="2" class="fondo"></li>
+                <li data-target="#myCarousel" data-slide-to="3" class="fondo"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner d-flex justify-content-center align-items-center">
+                <div class="item active">
+                    <a href="productos.php">
+                        <img src="camiseta.jpg" alt="" style="width:500px;height:300px">
+                    </a>
+                </div>
+                <div class="item">
+                    <a href="productos.php">
+                        <img src="zapatos.jpg" alt="" style="width:500px;height:300px">
+                    </a>
+                </div>
+                <div class="item">
+                    <a href="productos.php">
+                        <img src="pantalon.jpg" alt="" style="width:500px;height:300px">
+                    </a>
+                </div>
+                <div class="item">
+                    <a href="productos.php">
+                        <img src="sombrero.jpg" alt="" style="width:500px;height:300px">
+                    </a>
+                </div>
+            </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    </div>
 </body>
+<footer class="bg-light text-center text-lg-start mt-5">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        Carlos Gómez - Brendan Rodríguez - Víctor Cuevas © 2020 Copyright:
+        <a class="text-dark" href="portada.php">gorovi.com</a>
+    </div>
+    <!-- Copyright -->
+</footer>
 </html>
--- codigo/productos.php
+++ codigo/productos.php
@@ -1,50 +1,65 @@
 <?php
 require_once "funciones.php";
-    if (isset($_POST["añadir"])) {
-        $producto=$_POST["oculto"];
-        $_SESSION["cesta"][]=getProducto($producto);
-    }
+if (isset($_POST["añadir"])) {
+    $producto = $_POST["oculto"];
+    $_SESSION["cesta"][] = getProducto($producto);
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" media="screen">
+    <link rel="stylesheet" href="estilos.css">
     <title>Productos</title>
 </head>
+
 <body>
-    <nav>
-        <a href="portada.php">Inicio</a>
+    <nav class="navbar navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand px-5 volver" href="portada.php">Volver al inicio</a>
     </nav>
-    
-    <table border="1">
-        <thead>Productos</thead>
-        <th>Imagen</th><th>Nombre</th><th>Categoria</th><th>Precio</th>
-        <?php foreach(getProductos() as $producto) :?>
-            <tr>
-                <td>
-                    <img src="<?= $producto["nombre"] ?>" alt="<?= $producto["imagen"] ?>">
-                </td>
-                <td>
-                    <?= $producto["nombre"] ?>
-                </td>
-                <td>
-                    <?= $producto["categoria"] ?>
-                </td>
-                <td>
-                    <?= $producto["precio"] ?>
-                </td>
-                <?php if(isset($_SESSION["usuario"])) :?>
-                    <td>
-                        <form action="" method="POST">
-                            <input type="hidden" name="oculto" value="<?=$producto["id"]?>">
-                            <input type="submit" name="añadir" value="Añadir a la cesta">
-                        </form>
-                    </td>
-                <?php endif ;?>
-            </tr>
-        <?php endforeach ;?>
-    </table>
+
+    <div class="container" id="main">
+        <div class="row mx-0 mt-5">
+            <?php foreach (getProductos() as $producto) : ?>
+                <div class="col-12 col-lg-3 m-0 d-flex py-2">
+                    <div class="w-100 h-100 d-flex flex-column fondo-producto shadow border rounded">
+                        <div class="row mx-0 no-gutters w-100">
+                            <h3 class="text-center nombre-producto"> <?= $producto["nombre"] ?> </h3>
+                        </div>
+                        <div class="row mx-0 no-gutters d-flex flex-column w-100">
+                            <img src="homer.jpg" alt="" class="img-fluid row mx-0 no-gutters">
+                            <div class="row mx-0 d-flex flex-row mx-0 no-gutters py-2">
+                                <div class="col-12 col-lg-6 justify-content-center align-items-center d-flex text-truncate">Categoría: <?= $producto["categoria"] ?></div>
+                                <div class="col-12 col-lg-6 justify-content-center align-items-center d-flex text-truncate">Precio: <?= $producto["precio"] ?>€</div>
+                            </div>
+                            <div class="row mx-0 justify-content-center align-items-center d-flex py-2">
+                                <?php if (isset($_SESSION["usuario"])) : ?>
+                                    <form action="" method="POST" class="d-flex justify-content-center align-items-center">
+                                        <input type="hidden" name="oculto" value="<?= $producto["id"] ?>">
+                                        <input type="submit" name="añadir" value="Añadir a la cesta" class="btn btn-primary botones" type="button">
+                                    </form>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+    <footer class="bg-light text-center text-lg-start mt-5">
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            Carlos Gómez - Brendan Rodríguez - Víctor Cuevas © 2020 Copyright:
+            <a class="text-dark" href="portada.php">gorovi.com</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    </div>
 </body>
+
 </html>
+<!-- <img src="<?= $producto["nombre"] ?>" alt="<?= $producto["imagen"] ?>" class="img-responsive">-->
